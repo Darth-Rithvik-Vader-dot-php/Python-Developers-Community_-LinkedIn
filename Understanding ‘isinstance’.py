5ad8246fd2ac630b859d6c68f459721a5c21f12b
Darth-Rithvik-Vader-dot-php <144507773+Darth-Rithvik-Vader-dot-php@users.noreply.github.com>
2024-12-23 21:43:05 -0500

    Update Understanding ‘isinstance’.py
    Completed the coding of ‘isinstance’.py

--- Understanding ‘isinstance’.py
+++ Understanding ‘isinstance’.py
@@ -1,45 +1,35 @@
+## first example
 # type matching - Buildin Types
 a = 10
 print isinstance(a, int)) # True
 b = 10.1
 print(isinstance(b, int)) # Flase
-1
+
+## second example
 # Checks from tuple of types
-2
 # True only if one of the type is matched
-3
 a = {'k': ' value'}
-4 print(isinstance(a, (dict, list))) #True
-5
-b = [1,2,31
-6
+print(isinstance(a, (dict, list))) #True
+b = [1,2,3]
 print (isinstance(b, (dict, list))) #True
-7
 c= 10
-8
 print(isinstance(c, (dict, list))) #False
-1
+
+## third example
 # none type check
-2
 a = None
-3
 print (isinstance(a, type(None)))
-1 # inheritance checks are done with isinstance
-2 class A:
-3
-pass
-4
-5
+
+## fourth example
+# inheritance checks are done with isinstance
+class A:
+    pass
+
 class B(A):
-6
-pass
-7
-8
+    pass
+
 a = A()
-9
-10
+b = B()
 print(isinstance(a,A)) #True
-11
 print(isinstance(b,A)) #True
-12
 print (isinstance(a,B)) #False
